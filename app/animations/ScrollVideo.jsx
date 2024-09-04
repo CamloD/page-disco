068ad60bc77e6bf535742116c83edc199d17fc65
CamloD <sergado9@gmail.com>
2024-09-04 13:45:31 -0500

    version 1.0

--- app/animations/ScrollVideo.jsx
+++ app/animations/ScrollVideo.jsx
@@ -1,61 +1,43 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion, useInView, useScroll } from 'framer-motion';
 import styles from "@/app/Styles/ParallaxVideo.css"
 
 
 const ScrollVideo = () => {
-  const { scrollY } = useScroll();
-  const videoRef = React.createRef();
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <div className="video-container1">
-      <div className="video-background1" ref={videoRef}>
-        <motion.video
-          src="video.mp4"
-          autoPlay
-          loop
-          muted
-          playsInline
-          style={{
-            objectFit: 'cover',
-            width: '100%',
-            height: '100vh',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-          }}
-          y={scrollY * 0.5}
-          transition={{ duration: 0.5 }}
-        ></motion.video>
+    <div className="video-container1" style={{ position: 'relative' }}>
+      <div className="">
+      <motion.video
+  src="video.mp4"
+  autoPlay
+  loop
+  muted
+  playsInline
+  style={{
+    objectFit: 'cover',
+    width: '100%',
+    height: '100vh',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    transform: `translateY(${scrollY * 0.5}px)`, // Actualizado para que el video se mueva según la posición del scroll
+  }}
+  transition={{ duration: 0.5 }}
+></motion.video>
       </div>
-      <style>
-        {`
-          .video-container1 {
-            position: relative;
-            height: 100vh;
-            overflow: hidden;
-          }
-
-          .video-background1 {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            overflow: hidden;
-          }
-
-          .video-background1 video {
-            object-fit: cover;
-            width: 100%;
-            height: 100%;
-            position: absolute;
-            top: 0;
-            left: 0;
-          }
-        `}
-      </style>
     </div>
   );
 };
