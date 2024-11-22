5f01b1a82f0ac4f5c88984f2de0291dd2069cd12
CamloD <sergado9@gmail.com>
2024-11-22 14:21:49 -0500

    version 1.2.0

--- app/Dulcinea/animations/ImageBackgroung1.jsx
+++ app/Dulcinea/animations/ImageBackgroung1.jsx
@@ -10,7 +10,7 @@ const ImageBackground1 = () => {
   const intervalRef = useRef(null);
 
   const backgrounds = [
-    "linear-gradient(to right, #000000 0%, #737373 100%)",
+    //"linear-gradient(to right, #000000 0%, #737373 100%)",
     "url('images1.jpg')",
     "url('images/image1.jpg')",
     "url('images/image2.jpg')",
