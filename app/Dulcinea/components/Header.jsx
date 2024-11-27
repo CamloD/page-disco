308f72f8174d431d89b116232a81a89f071c0325
CamloD <sergado9@gmail.com>
2024-11-27 18:02:35 -0500

    version 1.2.2.7.3

--- app/Dulcinea/components/Header.jsx
+++ app/Dulcinea/components/Header.jsx
@@ -59,7 +59,7 @@ const Header = () => {
   };
 
   const getImagePath = (path) => {
-    return `${basePath}/${path}`.replace(/\/+/g, '/');
+    return `${basePath}${path}`.replace(/\+/g, '/');
   };
 
   return (
