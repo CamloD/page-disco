9603876a1d49d9b2c497f79fc19d31eb3ee108d7
CamloD <sergado9@gmail.com>
2024-11-27 17:37:51 -0500

    version 1.2.2.7.2

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-	/*output: 'export', */
+	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+	/*output: 'export',*/
 	images: {
-	  domains: ['example.com'],
 	  unoptimized: true,
 	},
-  };
-  module.exports = nextConfig
+  }
+  
   export default nextConfig;
+  
   
