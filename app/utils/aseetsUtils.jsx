497bbb874bd99cf1d0bca86c5af4147f18cf8e2a
CamloD <sergado9@gmail.com>
2024-11-28 16:22:00 -0500

    version 1.2.2.8.3

--- app/utils/aseetsUtils.jsx
+++
@@ -1,7 +0,0 @@
-export function getAssetPath(path) {
-    // Asegúrate de que esto coincida con la configuración de tu proyecto en GitHub Pages
-    const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-    return `${basePath}/${path.replace(/^\//, '')}`;
-  }
-  
-  
