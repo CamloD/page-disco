f894a54404ab3ef03da3a6a1f888e2931a40c419
CamloD <sergado9@gmail.com>
2024-12-14 00:42:46 -0500

    version 1.2.4.9

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true, // Este es el modo estricto de React
+	/*reactStrictMode: true, // Este es el modo estricto de React
 	swcMinify: true, // Usar SWC para minimizar el código
 	output: 'export', // Si estás utilizando el modo de exportación estática
 	experimental: {
 	  appDir: true, // Habilitar el enrutamiento de la carpeta app
-	},
+	},*/
 	basePath: process.env.NODE_ENV === 'production' ? '/page-disco' : '',
 	assetPrefix: process.env.NODE_ENV === 'production' ? '/page-disco/' : '',
 	images: {
